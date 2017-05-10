680f69ebbcbfcfdea05aab62b9c7878cccaeb392
wenpeng-w <449202631@qq.com>
2017-05-10 20:18:55 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -11,48 +11,32 @@ import 'common/stylus/index.css';
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-//const routes = [
-//	{
-//		path: '/',
-//		component: goods
-//	},
-//	{
-//		path: '/goods',
-//		component: goods
-//	},
-//	{
-//		path: '/ratings',
-//		component: ratings
-//	},
-//	{
-//		path: '/seller',
-//		component: seller
-//	}
-//];
-//
-//const router = new VueRouter({
-//	linkActiveClass: 'active',
-//	routes
-//});
-
-/* eslint-disable no-new */
-//new Vue({
-//	el: '#app',
-//	router,
-//	render: h => h(App)
-//});
-
 let routes = [
-  {path: '/', name: 'index', component: App, children: [{path: '/goods', component: goods}, {path: '/ratings', component: ratings}, {path: '/seller', component: seller}]}
+	{
+		path: '/',
+		component: goods
+	},
+  {
+		path: '/goods',
+		component: goods
+	},
+	{
+		path: '/ratings',
+		component: ratings
+	},
+	{
+		path: '/seller',
+		component: seller
+	}
 ];
 let router = new VueRouter({
   'linkActiveClass': 'active',
    routes
 });
-let app = new Vue({
-  router
-}).$mount('#app');
 
-router.push('/goods');
-
-export default app;
+/* eslint-disable no-new */
+new Vue({
+	el: '#app',
+	router,
+	render: h => h(App)
+});
