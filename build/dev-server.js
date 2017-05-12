46476069c2a11d008f536302cf1fb1ce66b688da
wenpeng-w <449202631@qq.com>
2017-05-12 14:05:52 +0800

    Update

--- build/dev-server.js
+++ build/dev-server.js
@@ -22,7 +22,7 @@ var proxyTable = config.dev.proxyTable
 
 var app = express()
 
-var appData = require('../data.json');
+var appData = require('../static/data.json');
 var seller = appData.seller;
 var goods = appData.goods;
 var ratings = appData.ratings;
